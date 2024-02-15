9fb7971d4ce45e049c13ed94959824a1511e83bd
Topallaj Denis <denis.topallaj@student.howest.be>
2024-02-15 16:44:48 +0100

    added: pdb-tools-examples folder. This is used to test out the pdb-tools functions. This consists of two python files: main.py and create_pdb.py added: main.py which contains the variables to call the create_pdb function with. added: create_pdb.py which contains the code to transform a given sequence into a .pdb (Protein Data Bank) file. These files are stored inside the pdb_files folder. added: gitignore and requirements as asked

---
+++ pdb-tools-examples/main.py
@@ -0,0 +1,7 @@
+from create_pdb_file import create_pdb
+
+# Create a PDB file for the protein sequence
+protein_sequence = "MKTITLEVAVLAALLVLASATVA"
+protein_name = "SomeProtein"
+
+create_pdb(protein_sequence, protein_name)
