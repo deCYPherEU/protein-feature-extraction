68deb356791d616462b0c12c8fbf1249036d3d4b
Topallaj Denis <denis.topallaj@student.howest.be>
2024-02-17 23:16:51 +0100

    Generating Biopython component with features Create the experiments folder to keep all past examples Create the project structure for fondant (pipeline.py, data folder, components folder ...). Copied a lot from the experiments/fondant-examples folder. Create containerized component for the Biopython functions. Generates 13 new features. Problems with the write_to_file component, it doesn't seem to work, can't find the columns of the dataframe, while being able to log them fully. Will have to look for answers.

---
+++ pipeline.py
@@ -0,0 +1,43 @@
+import pyarrow as pa
+from fondant.pipeline import Pipeline
+from components.generate_data import generate_mock_data
+from components.constants import MOCK_DATA_PATH_FONDANT
+from components.constants import FEATURES_GENERATED_OBJECT
+
+# generate the mock data, also creates a parquet file
+generate_mock_data()
+
+# create a new pipeline
+pipeline = Pipeline(
+	name="feature_extraction_pipeline",
+	base_path=".fondant",
+	description="A pipeline to extract features from protein sequences."
+)
+
+# read the dataset
+dataset = pipeline.read(
+	"load_from_parquet",
+	arguments={
+		"dataset_uri": MOCK_DATA_PATH_FONDANT,
+	},
+	produces={
+		"sequence": pa.string()
+	}
+)
+
+# apply the biopython component
+biopython_dataset = dataset.apply(
+    "./components/biopython_component",
+)
+
+# write the dataset to a parquet file
+biopython_dataset.apply(
+	"write_to_file",
+	arguments={
+		"path": "/data/export",
+	},
+	consumes=FEATURES_GENERATED_OBJECT
+)
+
+# run the pipeline using your local path to the folder, this one is mine
+# fondant run local pipeline.py --extra-volumes C:\Users\denis\Desktop\stage\protein-feature-extraction\data:/data
