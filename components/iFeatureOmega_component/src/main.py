204131d903ec058d7ffcdb6db70cf9fe9c71e09a
Topallaj Denis <denis.topallaj@student.howest.be>
2024-03-20 16:37:30 +0100

    Added the github actions back The biopython and iFeatureOmega component have been modified, because the pylint says so. Had to bump the python version down from 3.11 to 3.10, because it otherwise doesn't work. Problem with Nimbus.

--- components/iFeatureOmega_component/src/main.py
+++ components/iFeatureOmega_component/src/main.py
@@ -1,64 +1,74 @@
+"""
+The IFeatureOmegaComponent class is a component that generates new features using iFeatureOmega.
+"""
+
 import logging
+import uuid
 import pandas as pd
 from fondant.component import PandasTransformComponent
 import iFeatureOmega_CLI.iFeatureOmegaCLI as iFO
-import uuid
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
+
 class IFeatureOmegaComponent(PandasTransformComponent):
-	"""The IFeatureOmegaComponent class is a component that generates new features using iFeatureOmega."""
+    """The IFeatureOmegaComponent class is a component that generates new features using iFeatureOmega."""
 
-	def __init__(self, descriptors: list):
-		self.descriptors = descriptors
+    def __init__(self, descriptors: list):
+        self.descriptors = descriptors
 
-	def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-		# Get the sequence from the dataframe
-		sequences = dataframe["sequence"].tolist()
-		
-		# Generate all features, only need one to get the column names
-		all_features = self.generate_all_features_names(sequences[0])
+    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """Perform the iFeatureOmega functions to generate new features and return the dataframe with the new features added."""
 
-		# Add the columns to the dataframe
-		dataframe = pd.concat([dataframe, pd.DataFrame(columns=all_features)])
+        # Get the sequence from the dataframe
+        sequences = dataframe["sequence"].tolist()
 
-		# Generate the iFeatureOmega features
-		dataframe = self.generate_ifeature_omega_values(sequences, dataframe)
+        # Generate all features, only need one to get the column names
+        all_features = self.generate_all_features_names(sequences[0])
 
-		return dataframe
+        # Add the columns to the dataframe
+        dataframe = pd.concat([dataframe, pd.DataFrame(columns=all_features)])
 
-	def generate_ifeature_omega_values(self, sequences: list, dataframe: pd.DataFrame) -> pd.DataFrame:
-		# for each descriptor, generate the values for one sequence
-		for sequence in sequences:
-			ifeatureO_protein = self.create_ifo_protein(sequence)
-			for descriptor in self.descriptors:
-				ifeatureO_protein.get_descriptor(descriptor)
-				df_ifeature_protein = ifeatureO_protein.encodings
-				dataframe.loc[dataframe["sequence"] == sequence, df_ifeature_protein.columns] = df_ifeature_protein.values[0]
-		
-		return dataframe
+        # Generate the iFeatureOmega features
+        dataframe = self.generate_ifeature_omega_values(sequences, dataframe)
 
-	def create_ifo_protein(self, sequence: str) -> iFO.iProtein:
-		file_name = uuid.uuid4()
-		file_path = f"/{file_name}.txt"
-		with open(file_path, "w") as file:
-			file.write(f">{file_name}\n")
-			file.write(sequence)
-		
-		ifeatureO_protein = iFO.iProtein(file_path)
+        return dataframe
 
-		return ifeatureO_protein
-	
-	def generate_all_features_names(self, sequence: str) -> list:
-		all_features = []
+    def generate_ifeature_omega_values(self, sequences: list, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """Generate the iFeatureOmega features for the sequences and add them to the dataframe."""
+        # for each descriptor, generate the values for one sequence
+        for sequence in sequences:
+            ifeature_omega_protein = self.create_ifo_protein(sequence)
+            for descriptor in self.descriptors:
+                ifeature_omega_protein.get_descriptor(descriptor)
+                df_ifeature_protein = ifeature_omega_protein.encodings
+                dataframe.loc[dataframe["sequence"] == sequence,
+                              df_ifeature_protein.columns] = df_ifeature_protein.values[0]
 
-		single_protein = self.create_ifo_protein(sequence)
+        return dataframe
 
-		for descriptor in self.descriptors:
-			single_protein.get_descriptor(descriptor)
-			df_ifeature_protein = single_protein.encodings
-			all_features.extend(df_ifeature_protein.columns)
+    def create_ifo_protein(self, sequence: str) -> iFO.iProtein:
+        """Create an iFeatureOmega protein object from a sequence."""
+        file_name = uuid.uuid4()
+        file_path = f"/{file_name}.txt"
+        with open(file_path, "w") as file:
+            file.write(f">{file_name}\n")
+            file.write(sequence)
 
-		return all_features
+        ifeature_omega_protein = iFO.iProtein(file_path)
 
+        return ifeature_omega_protein
+
+    def generate_all_features_names(self, sequence: str) -> list:
+        """Generate all the features names for the descriptors."""
+        all_features = []
+
+        single_protein = self.create_ifo_protein(sequence)
+
+        for descriptor in self.descriptors:
+            single_protein.get_descriptor(descriptor)
+            df_ifeature_protein = single_protein.encodings
+            all_features.extend(df_ifeature_protein.columns)
+
+        return all_features
