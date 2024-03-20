ac3f4c837dc3ecb67fec557fb7e9a1b45176a4bc
Topallaj Denis <denis.topallaj@student.howest.be>
2024-03-20 10:46:40 +0100

    Add init and notebook

--- utils/check_output.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# load parquet data from a folder and return a pandas dataframe
-def load_from_parquet(dataset_uri: str) -> pd.DataFrame:
-	return pd.read_parquet(dataset_uri)
-
-
-df = load_from_parquet('./data/export/')
-print(df.head())
