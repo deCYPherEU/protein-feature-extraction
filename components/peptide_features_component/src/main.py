e492ec9a82aaf0976b78e66dc933361dfa88efd0
Topallaj Denis <denis.topallaj@student.howest.be>
2024-03-26 15:16:47 +0100

    rebased

--- components/peptide_features_component/src/main.py
+++ components/peptide_features_component/src/main.py
@@ -5,6 +5,7 @@ import logging
 import pandas as pd
 from fondant.component import PandasTransformComponent
 import peptides
+import re
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -24,11 +25,19 @@ class PeptideFeaturesComponent(PandasTransformComponent):
 		
 		dataframe = self.calculate_aa_fractions(dataframe)	
 		dataframe = self.calculate_mz(dataframe)
+		dataframe = self.calculate_tmd_length(dataframe)
+		dataframe = self.calculate_tmd_hydrophobic_aa_fraction(dataframe)
+		dataframe = self.calculate_tmd_start_end_positions(dataframe)
+
 
 		logger.info(f"PeptideFeaturesComponent: features generated: {dataframe.columns.tolist()}")
 		return dataframe
 	
 	def calculate_scales(self, dataframe: pd.DataFrame, scale_method: str) -> pd.DataFrame:
+		"""
+		Calculate the z-scales, vhse-scales, t-scales and st-scales of a peptide sequence.
+		"""
+		
 		index = 1
 		for sequence in dataframe["sequence"]:
 			peptide = peptides.Peptide(sequence)
@@ -47,6 +56,10 @@ class PeptideFeaturesComponent(PandasTransformComponent):
 		return dataframe
 	
 	def calculate_mz(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+		"""
+		Calculate the mass-to-charge ratio (m/z) of a peptide sequence.
+		"""
+
 		for sequence in dataframe["sequence"]:
 			peptide = peptides.Peptide(sequence)
 			mz = peptide.mz()
@@ -74,4 +87,42 @@ class PeptideFeaturesComponent(PandasTransformComponent):
 			column_name = f"{category}_aa_fraction"
 			dataframe[column_name] = dataframe["sequence"].apply(lambda x: sum(x.count(aa) for aa in amino_acids) / len(x))
 		
+		return dataframe
+	
+	def calculate_tmd_length(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+		"""
+		Calculate the length of transmembrane domains (TMDs) in a protein sequence.
+		"""
+		
+		for sequence in dataframe["sequence"]:
+			tmd_pattern='[AILMFWV]{4,}'
+			tmd_lengths = [len(match.group()) for match in re.finditer(tmd_pattern, sequence)]
+			dataframe["tmd_length"] = sum(tmd_lengths)
+
+		return dataframe
+	def calculate_tmd_hydrophobic_aa_fraction(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+		"""
+		Calculate the fraction of hydrophobic amino acids in transmembrane domains (TMDs) of a protein sequence.
+		"""
+
+		for sequence in dataframe["sequence"]:
+			tmd_pattern='[AILMFWV]{4,}'
+			tmd_sequence = ''.join(match.group() for match in re.finditer(tmd_pattern, sequence))
+			hydrophobic_aa = 'AILMFWV'
+			hydrophobic_count = sum(tmd_sequence.count(aa) for aa in hydrophobic_aa)
+			dataframe["tmd_hydrophobic_aa_fraction"] = hydrophobic_count / len(tmd_sequence) if len(tmd_sequence) > 0 else 0.0
+		
+		return dataframe
+
+	def calculate_tmd_start_end_positions(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+		"""
+		Calculate the start and end positions of transmembrane domains (TMDs) in a protein sequence.
+		"""
+
+		for sequence in dataframe["sequence"]:
+			tmd_pattern = '[AILMFWV]{4,}'
+			tmd_positions = [(match.start(), match.end()) for match in re.finditer(tmd_pattern, sequence)]
+			dataframe["tmd_start_position"] = tmd_positions[0][0] + 1 if tmd_positions else None
+			dataframe["tmd_end_position"] = tmd_positions[-1][1] if tmd_positions else None
+
 		return dataframe
