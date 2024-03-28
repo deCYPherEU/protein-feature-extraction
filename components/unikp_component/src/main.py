88a215d9e4d76829adb1700aad69542359480770
Topallaj Denis <denis.topallaj@student.howest.be>
2024-03-28 09:09:27 +0100

    Modified main based on lint

--- components/unikp_component/src/main.py
+++ components/unikp_component/src/main.py
@@ -6,10 +6,11 @@ for easy storage and retrieval in the dataframe.
 """
 import logging
 import os
-import requests
 import json
-from dotenv import load_dotenv
 import pandas as pd
+from dotenv import load_dotenv # pylint: disable=import-error
+import requests
+from requests.exceptions import RequestException
 from fondant.component import PandasTransformComponent
 
 # Load the environment variables
@@ -20,73 +21,76 @@ logger = logging.getLogger(__name__)
 
 
 class PredictProtein3DStructureComponent(PandasTransformComponent):
-    """
-    The UniKP component uses the UniKP framework to predict
-    kinetic properties (Km, Kcat and Vmax) of a protein sequence and a ligand SMILES string.
-    The results will be presented as a JSON, but in a string format
-    for easy storage and retrieval in the dataframe.
-    """
+	"""
+	The UniKP component uses the UniKP framework to predict
+	kinetic properties (Km, Kcat and Vmax) of a protein sequence and a ligand SMILES string.
+	The results will be presented as a JSON, but in a string format
+	for easy storage and retrieval in the dataframe.
+	"""
 
-    def __init__(self, protein_smiles_path: str):
-        self.hf_api_key = os.getenv("HF_API_KEY")
-        self.hf_endpoint_url = os.getenv("HF_ENDPOINT_URL")
-        self.empty_prediction = {
-            "Km": None,
-            "Kcat": None,
-            "Vmax": None
-        }
+	def __init__(self, protein_smiles_path: str):
+		# pylint: disable=super-init-not-called
 
-        self.protein_smiles_path = protein_smiles_path
+		self.hf_api_key = os.getenv("HF_API_KEY")
+		self.hf_endpoint_url = os.getenv("HF_ENDPOINT_URL")
+		self.empty_prediction = {
+			"Km": None,
+			"Kcat": None,
+			"Vmax": None
+		}
 
-        if not self.hf_api_key or not self.hf_endpoint_url:
-            raise Exception("environment variables not set.")
+		self.protein_smiles_path = protein_smiles_path
 
-    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        """Perform the transformation on the dataframe."""
+		if not self.hf_api_key or not self.hf_endpoint_url:
+			raise ValueError("environment variables not set.")
 
-        # load the json file
-        with open(self.protein_smiles_path, "r") as f:
-            data = json.load(f)
+	def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+		"""Perform the transformation on the dataframe."""
 
-        for protein_sequence, smiles_list in data.items():
-            predictions = {}
-            for smiles in smiles_list:
-                result = self.predict_kinetic_properties(
-                    protein_sequence, smiles)
-                if result is not None:
-                    predictions[smiles] = result
-                else:
-                    predictions[smiles] = self.empty_prediction
+		# load the json file
+		with open(self.protein_smiles_path, "r") as f:
+			data = json.load(f)
 
-            dataframe.loc[dataframe["sequence"] == protein_sequence, [
-                "unikp_kinetic_prediction"]] = str(json.dumps(predictions))
+		for protein_sequence, smiles_list in data.items():
+			predictions = {}
+			for smiles in smiles_list:
+				result = self.predict_kinetic_properties(
+					protein_sequence, smiles)
+				if result is not None:
+					predictions[smiles] = result
+				else:
+					predictions[smiles] = self.empty_prediction
 
-        return dataframe
+			dataframe.loc[dataframe["sequence"] == protein_sequence, [
+				"unikp_kinetic_prediction"]] = str(json.dumps(predictions))
 
-    def predict_kinetic_properties(self, protein_sequence: str, ligand_smiles: str) -> str:
-        """
-        Predict the kinetic properties of a protein sequence and a ligand SMILES string.
-        """
-        # Prepare the data
-        data = {
-            "inputs": {
-                "sequence": protein_sequence,
-                "smiles": ligand_smiles
-            }
-        }
+		return dataframe
 
-        # Make the API call
-        headers = {
-            "Authorization": f"Bearer {self.hf_api_key}",
-            "Content-Type": "application/json"
-        }
-        response = requests.post(self.hf_endpoint_url,
-                                 json=data, headers=headers)
+	def predict_kinetic_properties(self, protein_sequence: str, ligand_smiles: str) -> str:
+		"""
+		Predict the kinetic properties of a protein sequence and a ligand SMILES string.
+		"""
 
-        # Check if the request was successful
-        if response.status_code != 200:
-            raise Exception(
-                f"Request failed with status code {response.status_code} and response {response.text}")
+		data = {
+			"inputs": {
+				"sequence": protein_sequence,
+				"smiles": ligand_smiles
+			}
+		}
 
-        # Return the pdb string
-        return response.json()
+		try:
+			response = requests.post(
+				self.hf_endpoint_url, json=data, headers=self.get_headers(), timeout=30)
+			response.raise_for_status()
+		except RequestException as e:
+			logger.error("Request failed: %s", e)
+			return None
+
+		return response.json()
+
+	def get_headers(self):
+		"""Get headers for the API call."""
+		return {
+			"Authorization": f"Bearer {self.hf_api_key}",
+			"Content-Type": "application/json"
+		}
