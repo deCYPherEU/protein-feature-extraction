1451a28b8e7e7465be714d381955b79570404117
pietercoussement <coussementpieter@gmail.com>
2024-07-17 09:52:07 +0200

    update

--- components/unikp_component/src/main.py
+++ components/unikp_component/src/main.py
@@ -21,91 +21,95 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 
 
-class PredictProtein3DStructureComponent(PandasTransformComponent):
-	"""
-	The UniKP component uses the UniKP framework to predict
-	kinetic properties (Km, Kcat and Vmax) of a protein sequence and a ligand SMILES string.
-	The results will be presented as a JSON, but in a string format
-	for easy storage and retrieval in the dataframe.
-	"""
+class PredictEnyzmCharacteristicsComponent(PandasTransformComponent):
+    """
+    The UniKP component uses the UniKP framework to predict
+    kinetic properties (Km, Kcat and Vmax) of a protein sequence and a ligand SMILES string.
+    The results will be presented as a JSON, but in a string format
+    for easy storage and retrieval in the dataframe.
+    """
 
-	def __init__(self, protein_smiles_path: str):
-		# pylint: disable=super-init-not-called
+    def __init__(self, target_molecule_smiles: str):
+        # pylint: disable=super-init-not-called
 
-		self.hf_api_key = os.getenv("HF_API_KEY")
-		self.hf_endpoint_url = os.getenv("HF_ENDPOINT_URL")
-		self.empty_prediction = {
-			"Km": None,
-			"Kcat": None,
-			"Vmax": None
-		}
+        self.caller = hf_caller()
 
-		self.protein_smiles_path = protein_smiles_path
+        self.target_molecule_smiles = target_molecule_smiles
 
-		self.check_existence_of_files()
-
-		if not self.hf_api_key or not self.hf_endpoint_url:
-			raise ValueError("environment variables not set.")
+        self.check_existence_of_files()
 
 
-	def check_existence_of_files(self) -> None:
-		"""Check if the required files exist in the local_pdb_files_path directory."""
-
-		if not os.path.exists(self.protein_smiles_path):
-			logger.error(
-				"File %s not found. Please make sure the file exists.",
-				self.protein_smiles_path)
-			raise FileNotFoundError(
-				f"File {self.protein_smiles_path} not found. Please make sure the file exists.")
 
 
-	def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-		"""Perform the transformation on the dataframe."""
+    def check_existence_of_files(self) -> None:
+        """Check if the required files exist in the local_pdb_files_path directory."""
 
-		# load the json file
-		with open(self.protein_smiles_path, "r") as f:
-			data = json.load(f)
+        if not os.path.exists(self.target_molecule_smiles):
+            logger.error(
+                "File %s not found. Please make sure the file exists.",
+                self.target_molecule_smiles)
+            raise FileNotFoundError(
+                f"File {self.target_molecule_smiles} not found. Please make sure the file exists.")
 
-		for protein_sequence, smiles_list in data.items():
-			predictions = {}
-			for smiles in smiles_list:
-				result = self.predict_kinetic_properties(
-					protein_sequence, smiles)
-				if result is not None:
-					predictions[smiles] = result
-				else:
-					predictions[smiles] = self.empty_prediction
 
-			dataframe.loc[dataframe["sequence"] == protein_sequence, [
-				"unikp_kinetic_prediction"]] = str(json.dumps(predictions))
+    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """Perform the transformation on the dataframe."""
+        molecules = read_json_file(self.target_molecule_smiles)
 
-		return dataframe
+        dataframe['unikp_kinetic_prediction'] = dataframe.apply(
+            lambda x: predict_over_dataframe(
+                self.caller,
+                x["sequence"],
+                molecules),axis=1)
 
-	def predict_kinetic_properties(self, protein_sequence: str, ligand_smiles: str) -> str:
-		"""
-		Predict the kinetic properties of a protein sequence and a ligand SMILES string.
-		"""
+        return dataframe
 
-		data = {
-			"inputs": {
-				"sequence": protein_sequence,
-				"smiles": ligand_smiles
-			}
-		}
+class hf_caller():
+    def __init__(self) -> None:
+        self.hf_api_key = os.getenv("HF_API_KEY")
+        self.hf_endpoint_url = os.getenv("HF_ENDPOINT_URL")
 
-		try:
-			response = requests.post(
-				self.hf_endpoint_url, json=data, headers=self.get_headers(), timeout=30)
-			response.raise_for_status()
-		except RequestException as e:
-			logger.error("Request failed: %s", e)
-			return None
+        if not self.hf_api_key or not self.hf_endpoint_url:
+            raise ValueError("environment variables not set.")
 
-		return response.json()
+    def get_headers(self):
+        """Get headers for the API call."""
+        return {
+            "Authorization": f"Bearer {self.hf_api_key}",
+            "Content-Type": "application/json"
+        }
 
-	def get_headers(self):
-		"""Get headers for the API call."""
-		return {
-			"Authorization": f"Bearer {self.hf_api_key}",
-			"Content-Type": "application/json"
-		}
+    def predict_kinetic_properties(
+        self,
+        protein_sequence: str,
+        target_smile: str) -> dict:
+
+        data = {
+                    "inputs": {
+                        "sequence": protein_sequence,
+                        "smiles": target_smile
+                    }
+                }
+        try:
+            response = requests.post(
+                self.hf_endpoint_url, json=data, headers=self.get_headers(), timeout=30)
+            response.raise_for_status()
+        except RequestException as e:
+            logger.error("Request failed: %s", e)
+            return None
+
+        return response.json()
+
+def read_json_file(path):
+    with open(path, "r") as f:
+        data = json.load(f)
+    return data
+
+def predict_over_dataframe(hf, sequence, f):
+    out = {}
+    for name,smiles in f.items():
+        print(name, smiles)
+        result = hf.predict_kinetic_properties(sequence,smiles)
+        out[name] = result
+    return out
+
